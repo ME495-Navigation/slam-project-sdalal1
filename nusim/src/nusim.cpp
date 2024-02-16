a5b9984464ab501f20382ccd97fa90b37745f148
sdalal <shaildalal2024@u.northwestern.edu>
2024-02-15 19:42:34 -0600

    noise in wheel

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -29,6 +29,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <random>
 
 #include "std_msgs/msg/u_int64.hpp"
 #include "tf2/LinearMath/Quaternion.h"
@@ -46,6 +47,7 @@
 #include "turtlelib/diff_drive.hpp"
 #include "turtlelib/geometry2d.hpp"
 #include "nav_msgs/msg/path.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 using namespace std::chrono_literals;
 
@@ -68,8 +70,10 @@ public:
     declare_parameter("motor_cmd_per_rad_sec", 0.024);
     declare_parameter("wheel_radius", 0.0);
     declare_parameter("track_width", 0.0);
-    // declare_parameter("input_noise", 0.0);
-    // declare_parameter("slip_fraction", 0.0);
+    declare_parameter("input_noise", 0.01);
+    declare_parameter("slip_fraction", 0.01);
+    declare_parameter("basic_sensor_variance", 0.01);
+    declare_parameter("max_range", 8.0); 
 
     rate = get_parameter("rate").as_double();
     x0 = get_parameter("x0").as_double();
@@ -83,10 +87,15 @@ public:
     motor_cmd_per_rad_sec_ = get_parameter("motor_cmd_per_rad_sec").as_double();
     wheel_radius_ = get_parameter("wheel_radius").as_double();
     track_width_ = get_parameter("track_width").as_double();
-    // input_noise_ = get_parameter("input_width").as_double();
-    // slip_fraction_ = get_parameter("slip_fraction").as_double();
+    input_noise_ = get_parameter("input_noise").as_double();
+    slip_fraction_ = get_parameter("slip_fraction").as_double();
+    basic_sensor_variance_ = get_parameter("basic_sensor_variance").as_double();
+    max_range_ = get_parameter("max_range").as_double();
+
     timer_ = create_wall_timer(
       1000ms / rate, std::bind(&Nusim::timer_callback, this));
+    fake_timer_ = create_wall_timer(
+      1s / 5, std::bind(&Nusim::fake_timer_callback, this));
     srv_reset =
       create_service<std_srvs::srv::Empty>(
       "~/reset",
@@ -111,6 +120,8 @@ public:
     sensor_pub = create_publisher<nuturtlebot_msgs::msg::SensorData>(
       "red/sensor_data",
       10);
+    laser_pub = create_publisher<sensor_msgs::msg::LaserScan>(
+      "red/laser", 10);
     tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     ground_frame_loc(x0, y0, theta0);
     tr = {turtlelib::Vector2D{x0, y0}, theta0};
@@ -126,9 +137,12 @@ private:
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr publisher_walls;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr publisher_obs;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::TimerBase::SharedPtr fake_timer_;
   // C.7 sub
   rclcpp::Subscription<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr red_wheel_sub;
   rclcpp::Publisher<nuturtlebot_msgs::msg::SensorData>::SharedPtr sensor_pub;
+
+  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr laser_pub;
 
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr red_path_pub;
   
@@ -144,6 +158,7 @@ private:
   geometry_msgs::msg::TransformStamped t = geometry_msgs::msg::TransformStamped();
   nav_msgs::msg::Path red_path = nav_msgs::msg::Path();
   geometry_msgs::msg::PoseStamped  ps = geometry_msgs::msg::PoseStamped();
+  sensor_msgs::msg::LaserScan laser = sensor_msgs::msg::LaserScan();
 
   double rate = 0.0;
   double x0 = 0.0;
@@ -163,6 +178,8 @@ private:
   double track_width_ = 0.16;
   double input_noise_ = 0.0;
   double slip_fraction_ = 0.0;
+  double basic_sensor_variance_ = 0.0;
+  double max_range_ = 0.0;
 
   turtlelib::Transform2D tr;
   std::unique_ptr<turtlelib::DiffDrive> diff;
@@ -299,6 +316,34 @@ private:
     t.transform.rotation.w = q.w();
   }
 
+  void publish_laser(){
+    laser.header.frame_id = "red/base_footprint";
+    laser.angle_min = -turtlelib::PI;
+    laser.angle_max = turtlelib::PI;
+    laser.angle_increment = 0.0174533; // in radians
+    laser.time_increment = 1;
+    laser.scan_time = 1;
+    laser.range_min = 0.16;
+    laser.range_max = 8.0;
+
+    laser_pub -> publish(laser);
+  }
+
+  std::mt19937 & get_random()
+  {
+    // static variables inside a function are created once and persist for the remainder of the program
+    static std::random_device rd{}; 
+    static std::mt19937 mt{rd()};
+    // we return a reference to the pseudo-random number genrator object. This is always the
+    // same object every time get_random is called
+    return mt;
+  }
+
+  void fake_timer_callback()
+  {
+    
+  }
+
   /// \brief Creates a timer to publish frames and timestep
   void timer_callback()
   {
@@ -309,17 +354,17 @@ private:
     publisher_timestep->publish(time_msg);
 
     red_sensor.stamp = this->get_clock()->now();
-    red_sensor.left_encoder = left_wheel;
-    red_sensor.right_encoder = right_wheel;
-
+    
     red_path.header.stamp = this->get_clock()->now();
 
     t.header.stamp = this->get_clock()->now();
     ps.header.stamp = this->get_clock()->now();
+    laser.header.stamp = this->get_clock()->now();
 
     tf_broadcaster_->sendTransform(t);
 
     sensor_pub->publish(red_sensor);
+
   }
 
   /// \brief The service to teleport the bot in rviz
@@ -345,15 +390,32 @@ private:
   /// \param the message to get published wheel commands
   void red_wheel_callback(const nuturtlebot_msgs::msg::WheelCommands::SharedPtr msg)
   {
-    new_wheels.left_velocity = msg->left_velocity;
-    new_wheels.right_velocity = msg->right_velocity;
+    auto left_wheel_velocity = (msg->left_velocity * motor_cmd_per_rad_sec_ / rate);
+    auto right_wheel_velocity = (msg->right_velocity * motor_cmd_per_rad_sec_ / rate);
+    std::normal_distribution<> d(0.0, input_noise_);
+    auto noise = d(get_random());
+    
+    auto slip_noise = std::uniform_real_distribution<>{-slip_fraction_, slip_fraction_};
+    
+    if(left_wheel_velocity != 0 ){
+      
+      left_wheel_velocity += noise;
+    }
+    if (right_wheel_velocity!=0)
+    {
+      right_wheel_velocity += noise;
+    }
+    left_wheel += left_wheel_velocity;
+    right_wheel +=  right_wheel_velocity;
 
-    left_wheel += new_wheels.left_velocity * motor_cmd_per_rad_sec_ / rate * 652.229299363;
-    right_wheel += new_wheels.right_velocity * motor_cmd_per_rad_sec_ / rate * 652.229299363;
+    red_sensor.left_encoder = left_wheel * 652.229299363;
+    red_sensor.right_encoder = right_wheel * 652.229299363;
+
+    auto slipping_noise = (1+ slip_noise(get_random()));
 
     diff->compute_fk(
-      new_wheels.left_velocity * motor_cmd_per_rad_sec_ / rate,
-      new_wheels.right_velocity * motor_cmd_per_rad_sec_ / rate);
+      slipping_noise * left_wheel_velocity,
+      slipping_noise * right_wheel_velocity);
     auto trans_red = diff->get_transformation();
     ground_frame_loc(trans_red.translation().x, trans_red.translation().y, trans_red.rotation());
 
@@ -371,7 +433,6 @@ private:
 
     red_path.poses.push_back(ps);
     red_path_pub -> publish(red_path);
-
 
   }
 
